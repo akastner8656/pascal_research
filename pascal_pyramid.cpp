d9064916cf71eae3c7fb867201967319a2852bf1
Andrew Kastner <andkas906@gmail.com>
2018-06-29 23:36:16 -0400

    Updated Pascal's Pyramid

--- pascal_pyramid.cpp
+++ pascal_pyramid.cpp
@@ -4,33 +4,39 @@ using namespace std;
 
 int main()
 {
-	int lastRow;
+	double multiplier;
+	double otherValuesInRow;
+	double colNumber;
+	double rowNumber;
+	double firstValueInRow;
+	int userInput;
 	
-	cout << "How many rows do you want to produce?";
-	cin >> lastRow;
+	cout << "How many lines would you like to produce?" << endl;
+	cin >> userInput;
+	cout << endl << endl << endl << endl;
 	
-	for (int columnNumber = 0; columnNumber < lastRow; columnNumber++)
+	for (rowNumber = 3; rowNumber < userInput + 3; rowNumber++)
 	{
-		lastRowColumnPlaceholder =
-		 //code to produce the columnNumber's value at lastRow of Pascal's triangle
-		
-		for (int rowNumber = 0; rowNumber < lastRow; rowNumber++)
-		{
-			pascalTriangleValue = 
-			//code to produce columnNumber's value at rowNumber of Pascal's triangle
-			for (int columnNumberInRow = 0; columnNumberInRow <= rowNumber; columnNumberInRow++)
+		for (colNumber = 0; colNumber <= rowNumber - 3; colNumber++)
+		{			
+			if (colNumber == 0)
 			{
-				pascalPyramidValue = LastRowColumnPlaceholder * pascalTriangleValue;
+				firstValueInRow = rowNumber * (rowNumber - 1);
+				cout << firstValueInRow << " ";
+			}
+			else if (colNumber < rowNumber)
+			{
+				multiplier = (rowNumber - colNumber - 1) / (colNumber + 1);
+				otherValuesInRow = firstValueInRow * multiplier;
+				firstValueInRow = otherValuesInRow;
+				cout << otherValuesInRow << " ";
+			}
+			else
+			{
+				cout << endl;
 			}
 		}
+		colNumber = 0;
+		cout << endl << endl;
 	}
 }
-
-//This is a skeleton of the process used to create Pascal's pyramid.
-
-//Any rowNumber in Pascal's triangle will be multiplied by a term in the lastRow of Pascal's triangle.
-//This term is equal to the rowNumber being altered.
-
-//Example: Row 6 of Pascal's triangle will be multiplied by the sixth term of the last row
-//in Pascal's triangle.  If the last row produced was row 16, the sixth term of row 16 would
-//be the scalar that multiplies into every element of row 6.
